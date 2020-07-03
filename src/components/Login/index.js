d0b5a0e10555305f8b6cce9afe29694b0e1316cd
Melissa Castorena <mcastorena0316@gmail.com>
2020-07-03 10:54:59 -0500

    se crea component Login completo

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -4,31 +4,28 @@ import './Login.css';
 const Login = () => (
   <div className="login">
     <h3>Login</h3>
-    <button type="button">
-      <i className="fa fa-facebook-official" aria-hidden="true" />
-      Facebook
-    </button>
-    <button type="button" className="google">
-      {' '}
-      <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="Google" />
-      Google
-    </button>
     <form>
-      <p>Username:</p>
+      <p className="form-p">Username</p>
       <input id="username" type="text" />
-
-      <p>Password:</p>
+      <p className="form-p">Password</p>
       <input id="password" type="password" />
-      <div>
-        <input id="remember" type="checkbox" checked="checked" className="remember" />
+      <div className="remember-div">
+        <input id="remember" type="checkbox" className="remember" />
         <p>Remember me</p>
       </div>
-      <button type="button">Login</button>
+      <button type="button" className="login-btn">Login</button>
     </form>
-    <span className="psw">
-      Forgot
-      <a href="#">password?</a>
-    </span>
+    <div className="button-div">
+      <button type="button">
+        <i className="fa fa-facebook-official" aria-hidden="true" />
+        Facebook
+      </button>
+      <button type="button" className="google">
+        {' '}
+        <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="Google" />
+        Google
+      </button>
+    </div>
   </div>
 );
 
